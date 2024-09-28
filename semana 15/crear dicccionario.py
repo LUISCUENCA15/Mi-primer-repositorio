edda474755d230b07114d75ef7fe4f7dc3e28083
LUISCUENCA15 <lecuencaa@uea.edu.ec>
2024-09-28 05:21:48 -0500

    crear diccionario

---
+++ semana 15/crear dicccionario.py
@@ -0,0 +1,23 @@
+# Crear el diccionario 'informacion_personal'
+informacion_personal = {
+    "nombre": "Luis Cuenca",
+    "edad": 35,
+    "ciudad": "Arenillas",
+    "profesion": "Ingeniero"
+}
+
+# Acceder y modificar el valor asociado a "ciudad"
+informacion_personal["ciudad"] = "Barcelona"
+
+# Agregar una nueva clave-valor "profesion"
+informacion_personal["profesion"] = "Desarrollador de Software"
+
+# Verificar si la clave "telefono" existe y agregarla si no
+if "telefono" not in informacion_personal:
+    informacion_personal["telefono"] = "0998141777"
+
+# Eliminar la clave "edad"
+del informacion_personal["edad"]
+
+# Imprimir el diccionario final
+print(informacion_personal)
